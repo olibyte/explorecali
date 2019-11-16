e53807593b77ecd5ac4ffb859717e5cdf281d374
Oliver <ocben1@gmail.com>
2019-11-16 14:53:16 +1100

    explore search resource

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -4,6 +4,8 @@ import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
     List<Tour> findByTourPackageCode(@Param("code") String code);
 }
