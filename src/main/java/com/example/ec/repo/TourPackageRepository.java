b2f1eef47f25984b0cc31649759e4fe5df027522
Oliver <ocben1@gmail.com>
2019-11-16 14:52:35 +1100

    explore search resource

--- src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -2,7 +2,8 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    TourPackage findByName(String name);
+    TourPackage findByName(@Param("name")String name);
 }
--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,8 +1,9 @@
 package com.example.ec.repo;
 
-
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(@Param("code") String code);
 }
