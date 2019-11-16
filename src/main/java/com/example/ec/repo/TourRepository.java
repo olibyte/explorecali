48ef27abfed9dc61f30e4922052e2ca41f28903f
Oliver <ocben1@gmail.com>
2019-11-16 14:58:49 +1100

    paging and sorting

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,11 +1,14 @@
 package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
